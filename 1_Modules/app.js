2c5b8b1ae2a047f4d1b8b4b037bb2fb630a6c614
srikanta0427 <srikanta0427@gmail.com>
2026-03-26 00:24:08 +0530

    Import additional functions from sum and sum_files

--- 1_Modules/app.js
+++ 1_Modules/app.js
@@ -2,6 +2,11 @@ const { lol, sum} = require('./sum'); // either you can put sum.js or sum , it a
 const {substraction, printName} = require('./substraction.js'); // either you can put sum.js or sum , it always serve as .js
 // console.log(x);
 
+const {add} = require('./sum.js')
+
+//we can get all those modules of sum_files by giving only folder path also
+const {getDivision, getReminder} = require('./sum_files/')
+
 var x = 40; // this will give error cause lol has been already declared in ./sum.js
 console.log(x);
 
@@ -12,3 +17,7 @@ console.log(getSum);
 console.log(printName("javscript is a beautiful language."));
 
 console.log(substraction(5,6));
+
+console.log(add(4,5,6));
+
+console.log(getDivision(5,6))
