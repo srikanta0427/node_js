e993267a65043b2946892b4a46dc12a987115865
srikanta0427 <srikanta0427@gmail.com>
2026-03-28 20:39:59 +0530

    Update comment for variable naming clarification
    Clarified comment explaining the purpose of renaming variable 'x' to 'lol'.

--- 1_Modules/sum.js
+++ 1_Modules/sum.js
@@ -1,9 +1,8 @@
 let x = 10;
-
 const sum = (a, b)=>{
     return a+b;
 }
 
 module.exports={lol:x, sum};
 
-// here why lol:x cause here we use let x suppose in another js imported this file and use same variable it gives error that's why we give different name to the varible 
+// here why lol:x because here we use let x suppose in another js imported this file and use same variable it gives error that's why we give different name to the varible 
